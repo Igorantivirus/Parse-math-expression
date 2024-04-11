31773beb3340bb8ef93e93d5dcd6b80c9ae6d4d8
Igorantivirus <125749625+Igorantivirus@users.noreply.github.com>
2024-04-11 23:51:04 +0700

    Создан парсер
    Сделано:
    Добавлен класс парсера
    Создана обработка выражения
    Проблемы:
    возможны вылеты  ошибки (нет тестинга)
    не работает факториал

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 ﻿#include<iostream>
+
+#include<iostream>
 #include<vector>
 #include<cmath>
+#include<memory>
+#include <regex>
+#include <stack>
 
-#include<SFML\Network.hpp>
-#include<SFML\Graphics.hpp>
 #include<myio/out.hpp>
+#include<myio/in.hpp>
 
 #define PI_val 3.1415926535897932l
 
 using namespace out;
+using namespace in;
 
 using FType = double;
 
@@ -45,10 +50,10 @@ enum class FunctionType : char
 enum class Brackets : char
 {
 	none,
-	def,
-	round,
-	frac,
-	modul
+	def,	//default	()
+	round,	//round		[]
+	frac,	//fraction	{}
+	modul	//modul		||
 };
 
 bool firstAction(const Action a)
@@ -167,6 +172,7 @@ void outCloseBrack(const Brackets a)
 	}
 }
 
+
 class ExpressionBase
 {
 public:
@@ -177,7 +183,7 @@ public:
 	ExpressionBase() = default;
 	ExpressionBase(const Action& action) : next(action) {};
 
-	virtual std::unique_ptr<ExpressionBase> procession() = 0;
+	virtual std::unique_ptr<ExpressionBase> processed() const = 0;
 
 	const Action getNextAction() const
 	{
@@ -203,6 +209,7 @@ class Value : public ExpressionBase
 public:
 	Value() : ExpressionBase() {}
 	Value(const FType& nv) : ExpressionBase(), value(nv) {}
+	Value(const FType& nv, const Action na) : ExpressionBase(na), value(nv) {}
 	Value(const Value& other) : ExpressionBase(other.next), value(other.value) {}
 
 	Value& operator=(const Value& other)
@@ -221,7 +228,7 @@ public:
 		return *this;
 	}
 
-	std::unique_ptr<ExpressionBase> procession() override
+	std::unique_ptr<ExpressionBase> processed() const override
 	{
 		return std::make_unique<Value>(*this);
 	}
@@ -273,17 +280,15 @@ public:
 		return *this;
 	}
 
-	std::unique_ptr<ExpressionBase> procession() override
+	std::unique_ptr<ExpressionBase> processed() const override
 	{
 		if (arg == nullptr)
-			return std::make_unique<Value>(Value(0));
-
-		std::unique_ptr<ExpressionBase> pr = arg->procession();
+			return std::make_unique<Value>(Value(0, next));
+		std::unique_ptr<ExpressionBase> pr = arg->processed();
 		Value* valuePtr = nullptr;
 		if ((valuePtr = dynamic_cast<Value*>(pr.get())) == nullptr)
-			return std::make_unique<Value>(Value(0));
-
-		return std::make_unique<Value>(Value(processingFunction(valuePtr->getValue())));
+			return std::make_unique<Value>(Value(0, next));
+		return std::make_unique<Value>(Value(processingFunction(valuePtr->getValue()), next));
 	}
 
 	const FunctionType getType() const
@@ -326,7 +331,7 @@ private:
 		return std::make_unique<Function>(*this);
 	}
 
-	const FType processingFunction(const FType& v)
+	const FType processingFunction(const FType& v) const
 	{
 		switch (type)
 		{
@@ -364,8 +369,6 @@ private:
 class Expression : public ExpressionBase
 {
 public:
-	//friend class ExpressionProcesser;
-public:
 	Expression() : ExpressionBase() {}
 	Expression(const Expression& other) : ExpressionBase(other.next), brk(other.brk)
 	{
@@ -388,22 +391,51 @@ public:
 		return *this;
 	}
 
-	std::unique_ptr<ExpressionBase> procession()
+	std::unique_ptr<ExpressionBase> processed() const override
 	{
-		deleteNullptr();
+		std::vector<Value> vls;
+		
+		for (const auto& i : values)
+			if (i != nullptr)
+				vls.push_back(*dynamic_cast<Value*>(i->processed().get()));
 
-		for (size_t i = 0; i < values.size(); ++i)
-			values[i] = std::move(values[i]->procession());
+		vls.back().setNextAction(Action::none);
 
-		processActions();
+		firstProcessed(vls);
+		secondProcessed(vls);
+		thirdProcessed(vls);
 
+		if (brk == Brackets::round)
+			vls[0].setValue(std::round(vls[0].getValue()));
+		if (brk == Brackets::frac)
+		{
+			FType pr;
+			vls[0].setValue(std::modf(vls[0].getValue(), &pr));
+		}
+		if (brk == Brackets::modul)
+			vls[0].setValue(std::abs(vls[0].getValue()));
+		vls[0].setNextAction(next);
+		return std::make_unique<Value>(vls[0]);
+	}
 
-		return std::make_unique<Expression>(*this);
+	Value getProcessingResult() const
+	{
+		std::unique_ptr<ExpressionBase> expr = processed();
+		return *dynamic_cast<Value*>(expr.get());
 	}
 
 	void add(const ExpressionBase& expr)
 	{
 		values.push_back(std::move(expr.copy()));
+	}
+
+	const Brackets getBrackets() const
+	{
+		return brk;
+	}
+	void setBrackets(const Brackets nbr)
+	{
+		brk = nbr;
 	}
 
 	void print() const override
@@ -412,6 +444,7 @@ public:
 		for (const auto& i : values)
 			i->print();
 		outCloseBrack(brk);
+		outAct(next);
 	}
 
 private:
@@ -424,233 +457,385 @@ private:
 		return std::make_unique<Expression>(*this);
 	}
 
-	void deleteNullptr()
+	void firstProcessed(std::vector<Value>& vls) const
 	{
-		for (auto ptr = values.begin(); ptr != values.end();)
-			if (*ptr == nullptr)
-				ptr = values.erase(ptr);
-			else
-				++ptr;
-	}
-
-	void processActions()
-	{
-		processFirst();
-		processSecond();
-		processThird();
-	}
-
-	void processFirst()
-	{
-		for (size_t i = 0; i < values.size() - 1; ++i)
+		Value pr;
+		for (size_t i = 0; i < vls.size() - 1; ++i)
 		{
-			if (firstAction(values[i]->getNextAction()))
+			if (firstAction(vls[i].getNextAction()))
 			{
-				Value* ptr1 = dynamic_cast<Value*>(values[i].get());
-				Value* ptr2 = dynamic_cast<Value*>(values[i + 1].get());
-				Value res;
-				if (ptr1 != nullptr && ptr2 != nullptr)
-				{
-					if (ptr1->getNextAction() == Action::pow)
-						res.setValue(std::pow(ptr1->getValue(), ptr2->getValue()));
-					else if (ptr1->getNextAction() == Action::nrt)
-						res.setValue(std::pow(ptr1->getValue(), 1 / ptr2->getValue()));
-					else if (ptr1->getNextAction() == Action::log)
-						res.setValue(std::log(ptr1->getValue()) / std::log(ptr2->getValue()));
-					res.setNextAction(ptr2->getNextAction());
-				}
-				values[i] = std::move(std::make_unique<Value>(res));
-				values.erase(values.begin() + i + 1);
+				if (vls[i].getNextAction() == Action::pow)
+					pr.setValue(std::pow(vls[i].getValue(), vls[i+1].getValue()));
+				else if (vls[i].getNextAction() == Action::nrt)
+					pr.setValue(std::pow(vls[i].getValue(), 1 / vls[i + 1].getValue()));
+				else if (vls[i].getNextAction() == Action::pow)
+					pr.setValue(std::log(vls[i].getValue()) / std::log(vls[i + 1].getValue()));
+				pr.setNextAction(vls[i + 1].getNextAction());
+				vls[i] = pr;
+				vls.erase(vls.begin() + i + 1);
+				i--;
 			}
 		}
 	}
-	void processSecond()
+	void secondProcessed(std::vector<Value>& vls) const
 	{
-		for (size_t i = 0; i < values.size() - 1; ++i)
+		Value pr;
+		for (size_t i = 0; i < vls.size() - 1; ++i)
 		{
-			if (secondAction(values[i]->getNextAction()))
+			if (secondAction(vls[i].getNextAction()))
 			{
-				Value* ptr1 = dynamic_cast<Value*>(values[i].get());
-				Value* ptr2 = dynamic_cast<Value*>(values[i + 1].get());
-				Value res;
-				if (ptr1 != nullptr && ptr2 != nullptr)
-				{
-					if (ptr1->getNextAction() == Action::mult)
-						res.setValue(ptr1->getValue() * ptr2->getValue());
-					else if (ptr1->getNextAction() == Action::div)
-						res.setValue(ptr1->getValue() / ptr2->getValue());
-					else if (ptr1->getNextAction() == Action::rdiv)
-						res.setValue(std::fmod(ptr1->getValue(), ptr2->getValue()));
-					else if (ptr1->getNextAction() == Action::idiv)
-						res.setValue(std::round(ptr1->getValue() / ptr2->getValue()));
-					res.setNextAction(ptr2->getNextAction());
-				}
-				values[i] = std::move(std::make_unique<Value>(res));
-				values.erase(values.begin() + i + 1);
+				if (vls[i].getNextAction() == Action::mult)
+					pr.setValue(vls[i].getValue() * vls[i+1].getValue());
+				else if (vls[i].getNextAction() == Action::div)
+					pr.setValue(vls[i].getValue() / vls[i + 1].getValue());
+				else if (vls[i].getNextAction() == Action::rdiv)
+					pr.setValue(std::fmod(vls[i].getValue(), vls[i + 1].getValue()));
+				else if (vls[i].getNextAction() == Action::idiv)
+					pr.setValue(std::round(vls[i].getValue() /  vls[i + 1].getValue()));
+
+				pr.setNextAction(vls[i + 1].getNextAction());
+				vls[i] = pr;
+				vls.erase(vls.begin() + i + 1);
+				i--;
 			}
 		}
 	}
-	void processThird()
+	void thirdProcessed(std::vector<Value>& vls) const
 	{
-		for (size_t i = 0; i < values.size() - 1; ++i)
+		Value pr;
+		for (size_t i = 0; i < vls.size() - 1; ++i)
 		{
-			if (thirdAction(values[i]->getNextAction()))
+			if (thirdAction(vls[i].getNextAction()))
 			{
-				Value* ptr1 = dynamic_cast<Value*>(values[i].get());
-				Value* ptr2 = dynamic_cast<Value*>(values[i + 1].get());
-				Value res;
-				if (ptr1 != nullptr && ptr2 != nullptr)
-				{
-					if (ptr1->getNextAction() == Action::plus)
-						res.setValue(ptr1->getValue() + ptr2->getValue());
-					else if (ptr1->getNextAction() == Action::minus)
-						res.setValue(ptr1->getValue() - ptr2->getValue());
-					res.setNextAction(ptr2->getNextAction());
-				}
-				values[i] = std::move(std::make_unique<Value>(res));
-				values.erase(values.begin() + i + 1);
+				if (vls[i].getNextAction() == Action::plus)
+					pr.setValue(vls[i].getValue() + vls[i + 1].getValue());
+				else if (vls[i].getNextAction() == Action::minus)
+					pr.setValue(vls[i].getValue() - vls[i + 1].getValue());
+
+				pr.setNextAction(vls[i + 1].getNextAction());
+				vls[i] = pr;
+				vls.erase(vls.begin() + i + 1);
+				i--;
 			}
 		}
 	}
 
+};
 
+class ExpressionParser
+{
+public:
+
+	Expression parse(std::string str)
+	{
+		modulEdit(str);
+		std::vector<std::string> tokens;
+		tokenize(str, tokens);
+
+		Expression res;
+
+		for (size_t i = 0; i < tokens.size(); ++i)
+		{
+			if (isNum(tokens[i]))
+			{
+				Value prv = std::stold(tokens[i]);
+				if (i != tokens.size() - 1)
+					prv.setNextAction(parseAction(tokens[i++ + 1]));
+				res.add(prv);
+			}
+			else if (isFunc(tokens[i]))
+			{
+				Function prf;
+				prf.setType(parseFunction(tokens[i]));
+				prf.setArg(subParse(tokens[i + 1]));
+				if (i + 1 != tokens.size() - 1)
+					prf.setNextAction(parseAction(tokens[i++ + 2]));
+				res.add(prf);
+				i++;
+			}
+			else if (isOpenBracket(tokens[i][0]))
+			{
+				Expression pre = subParse(tokens[i]);
+				if (i != tokens.size() - 1)
+					pre.setNextAction(parseAction(tokens[i++ + 1]));
+				res.add(pre);
+			}
+		}
+
+
+		return res; return subParse(str);
+	}
+
+private:
+
+	Expression subParse(std::string str)
+	{
+		Brackets brk = parseBracket(str[0]);
+		if (brk != Brackets::none)
+		{
+			str.pop_back();
+			str.erase(str.begin());
+		}
+
+		std::vector<std::string> tokens;
+		tokenize(str, tokens);
 	
+		Expression res;
+		res.setBrackets(brk);
+	
+		for (size_t i = 0; i < tokens.size(); ++i)
+		{
+			if (isNum(tokens[i]))
+			{
+				Value prv = std::stold(tokens[i]);
+				if (i != tokens.size() - 1)
+					prv.setNextAction(parseAction(tokens[i++ + 1]));
+				res.add(prv);
+			}
+			else if(isFunc(tokens[i]))
+			{
+				Function prf;
+				prf.setType(parseFunction(tokens[i]));
+				prf.setArg(subParse(tokens[i + 1]));
+				if (i + 1 != tokens.size() - 1)
+					prf.setNextAction(parseAction(tokens[i++ + 2]));
+				res.add(prf);
+				i++;
+			}
+			else if (isOpenBracket(tokens[i][0]))
+			{
+				Expression pre = subParse(tokens[i]);
+				if(i != tokens.size() - 1)
+					pre.setNextAction(parseAction(tokens[i++ + 1]));
+				res.add(pre);
+			}
+		}
+
+
+		return res;
+	}
+
+private:
+
+	void modulEdit(std::string& str)
+	{
+		if (str[0] == '|')
+			str[0] = '<';
+		size_t pos;
+		while ((pos = str.find('|')) != std::string::npos)
+		{
+			if (!(str[pos - 1] >= '0' && str[pos - 1] <= '9') && str[pos - 1] != ')' && str[pos - 1] != ']' && str[pos - 1] != '}' && str[pos - 1] != '|')
+				str[pos] = '<';
+			else
+				str[pos] = '>';
+		}
+	}
+
+	void tokenize(const std::string& expression, std::vector<std::string>& tokens)
+	{
+		std::regex pattern("([a-zA-Z]+|\\d+(\\.\\d+)?|//|<[^>]+>|\\([^\\)]+\\)|\\[[^\\]]+\\]|\\{[^\\}]+\\}|[^a-zA-Z0-9])");
+		std::sregex_iterator words_begin = std::sregex_iterator(expression.begin(), expression.end(), pattern);
+		std::sregex_iterator words_end = std::sregex_iterator();
+		for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+		{
+			std::smatch match = *i;
+			for (size_t j = 1; j < match.size(); ++j)
+			{
+				if (!match[j].str().empty())
+				{
+					tokens.push_back(match[j].str());
+					break;
+				}
+			}
+		}
+	}
+
+private:
+
+	Action parseAction(const std::string& s)
+	{
+		switch (s[0])
+		{
+		case '+':
+			return Action::plus;
+		case '-':
+			return Action::minus;
+		case '*':
+			return Action::mult;
+		case '/':
+			return Action::div;
+		case '%':
+			return Action::rdiv;
+		case '^':
+			return Action::pow;
+		default:
+			if(s == "//")
+				return Action::idiv;
+			if(s == "log")
+				return Action::log;
+			if(s == "nrt")
+				return Action::nrt;
+			return Action::none;
+		}
+	}
+
+	FunctionType parseFunction(const std::string& s)
+	{
+		if (s == "sqrt")
+			return FunctionType::sqrt;
+		if (s == "ln")
+			return FunctionType::ln;
+		if (s == "lg")
+			return FunctionType::lg;
+		if (s == "sin")
+			return FunctionType::sin;
+		if (s == "cos")
+			return FunctionType::cos;
+		if (s == "tg")
+			return FunctionType::tg;
+		if (s == "ctg")
+			return FunctionType::ctg;
+		if (s == "arcsin")
+			return FunctionType::arcsin;
+		if (s == "arccos")
+			return FunctionType::arccos;
+		if (s == "arctg")
+			return FunctionType::arctg;
+		if (s == "arcctg")
+			return FunctionType::arcctg;
+		if (s == "fact")
+			return FunctionType::fact;
+		return FunctionType::none;
+	}
+
+	Brackets parseBracket(const char c)
+	{
+		if (c == '(')
+			return Brackets::def;
+		if (c == '[')
+			return Brackets::round;
+		if (c == '|')
+			return Brackets::modul;
+		if (c == '{')
+			return Brackets::frac;
+		return Brackets::none;
+	}
+
+private:
+
+	bool isNum(const std::string& str)
+	{
+		return str[0] >= '0' && str[0] <= '9';
+	}
+	bool isFunc(const std::string& str)
+	{
+		return str[0] >= 'a' && str[0] <= 'z';
+	}
+	bool isOpenBracket(const char c)
+	{
+		return c == '(' || c == '{' || c == '[' || c == '<';
+	}
+
+	char oppositeBracket(const char c)
+	{
+		switch (c)
+		{
+		case '(': return ')';
+		case ')': return '(';
+		case '[': return ']';
+		case ']': return '[';
+		case '{': return '}';
+		case '}': return '{';
+		case '<': return '>';
+		case '>': return '<';
+		default: return '\0';
+		}
+	}
+
+	bool goodBrackets(const std::vector<std::string>& arr)
+	{
+		std::stack<char> st;
+		for (const auto& i : arr)
+		{
+			if (isOpenBracket(i[0]))
+			{
+				if (st.top() == oppositeBracket(i[0]))
+					st.pop();
+				else
+					st.push(i[0]);
+
+			}
+		}
+		return st.empty();
+	}
 
 };
 
 int main()
 {
-	system("chcp 65001 > null");
-	
-	Value pr;
-	Expression ex;
+	system("chcp 65001 > nul");
 
-	pr = 156;
-	pr.setNextAction(Action::div);
-	ex.add(pr);
-	
-	pr = 12;
-	pr.setNextAction(Action::plus);
-	ex.add(pr);
-	
-	pr = -7.5;
-	pr.setNextAction(Action::rdiv);
-	ex.add(pr);
-	
-	pr = 0.5;
-	pr.setNextAction(Action::none);
-	ex.add(pr);
+	ExpressionParser parser;
+
+	while (true)
+	{
+		print(parser.parse(input()).getProcessingResult().getValue());
+	}
+
+	//Expression exp = parser.parse("(2+2)*2+sin(3.1415926/2)-[3.1415926]");
+	//exp.print();
+	//print(end, exp.getProcessingResult().getValue());
+
+	//print(tokenize("(2+2)*2"));
+	//print(tokenize("132+3*arccos(15.5)-999//35/9-<22-99>-(29+9)"));
+	//print(tokenize("2*(2+2)+sin(39+4!)"));
+	//print(tokenize("-<ab>ggg"));
+	//print(tokenize("+log15(25)"));
 
 
+	//Value v1;
+	//Value v2;
+	//Expression e1;
 
+	//v2 = 2;
+	//v2.setNextAction(Action::plus);
 
+	//v1.setNextAction(Action::pow);
 
+	//v1 = 10;
+	//e1.add(v1);
+	//e1.add(v2);
 
+	//v1 = 11;
+	//e1.add(v1);
+	//e1.add(v2);
 
-	ex.print();
-	std::cout << '\n';
-	auto ptr = ex.procession();
-	ptr->print();
+	//v1 = 12;
+	//e1.add(v1);
+	//e1.add(v2);
 
+	//v1 = 13;
+	//e1.add(v1);
+	//e1.add(v2);
 
+	//v1 = 14;
+	//e1.add(v1);
+	//e1.add(v2);
+
+	//e1.setBrackets(Brackets::def);
+
+	//Expression res;
+
+	//e1.setNextAction(Action::div);
+
+	//res.add(e1);
+
+	//res.add(Value(365));
+
+	//res.print();
+
+	//Value d = res.getProcessingResult();
+	//std::cout << '\n' << d.getValue() << '\n';
 
 	return 0;
 }
-
-
-////Сервер
-//sf::TcpListener listener;
-//sf::TcpSocket client;
-//
-//void sendMessage()
-//{
-//	std::string str;
-//	for (int i = 0; i < 10; ++i)
-//	{
-//		std::getline(std::cin, str);
-//		while (client.send(str.c_str(), str.size() + 1) != sf::Socket::Done) {}
-//	}
-//}
-//
-//void getMessage()
-//{
-//	char bytes[128]{};
-//	size_t received;
-//	for (int i = 0; i < 10; ++i)
-//	{
-//		while (client.receive(bytes, 128, received) != sf::Socket::Done) {}
-//			print(bytes, '\n');
-//	}
-//}
-//
-//int main()
-//{
-//	system("chcp 1251 > nul");
-//	
-//	if (listener.listen(53000) != sf::Socket::Done)
-//		print("Ошабка прослушивания\n");
-//
-//	if (listener.accept(client) != sf::Socket::Done)
-//		print("Ошибка привязки клиента\n");
-//	else
-//		print("Привязка клиента успеша!\n");
-//
-//	std::thread set(sendMessage);
-//	std::thread get(getMessage);
-//
-//	set.join();
-//	get.join();
-//
-//	return 0;
-//}
-
-
-
-
-//#define IP_SERVER_ADRESS "192.168.0.154"
-//
-//using namespace out;
-//
-////������
-//sf::TcpSocket socket;
-//sf::Socket::Status status;
-//
-//void sendMessage()
-//{
-//	std::string str;
-//	for (int i = 0; i < 10; ++i)
-//	{
-//		std::getline(std::cin, str);
-//		while (socket.send(str.c_str(), str.size() + 1) != sf::Socket::Done) {}
-//	}
-//}
-//
-//void getMessage()
-//{
-//	char bytes[128]{};
-//	size_t received;
-//	for (int i = 0; i < 10; ++i)
-//	{
-//		while (socket.receive(bytes, 128, received) != sf::Socket::Done) {}
-//		print(bytes, '\n');
-//	}
-//}
-//
-//int main()
-//{
-//	system("chcp 1251 > nul");
-//
-//	status = socket.connect(IP_SERVER_ADRESS, 53000);
-//	if (status != sf::Socket::Done)
-//		print("������ �����������\n");
-//	else
-//		print("����������� �������\n");
-//
-//	std::thread set(sendMessage);
-//	std::thread get(getMessage);
-//
-//	set.join();
-//	get.join();
-//
-//	return 0;
-//}
